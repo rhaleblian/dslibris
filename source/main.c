ab660eba6e5d37269df0e8791cd2dbe9c409acd9
rhaleblian <rhaleblian>
2007-07-10 14:26:01 +0000

    attempts at pagination. i will use pango instead.

--- source/main.c
+++ source/main.c
@@ -11,8 +11,10 @@ back page
 #include <ft2build.h> /* freetype2 - text rendering */
 #include FT_FREETYPE_H
 #include <expat.h>    /* expat - HTML parsing */
-
-#define BUFSIZE 10000
+#include <sys/stat.h>
+#include <unistd.h>
+	   
+#define BUFSIZE 32000
 #define MARGINLEFT 10
 #define MARGINRIGHT 10
 #define MARGINTOP 8
@@ -31,21 +33,55 @@ int Eventcnt = 0;
 FT_Library library;
 FT_Error   error;
 FT_Face    face;
-int	curchar;
+int	cursor, currentpage;
+int pageindex[2048];	// page indices into the char buffer
+int advance[128];		// glpyh advances for the current font
+char buf[BUFSIZE];
+
+int iswhitespace(char c) {
+	if(c == ' ' || c == '\t' || c == '\n') return 1;
+	return 0;
+}
+
+void paginate() {
+	long i;
+	int breakable = 0;
+	int xpixels = MARGINLEFT;
+	int line = 0;
+	
+	for(i=0;i<strlen(buf);i++) {
+		if(iswhitespace(buf[i])) breakable = i;
+		xpixels += advance[(int)buf[i]];
+		if(xpixels > SCREEN_WIDTH - MARGINLEFT - MARGINRIGHT) {
+			xpixels = 0;
+			line++;
+			if(line > 12) {
+				pageindex[currentpage++] = i;
+				line = 0;
+			}
+		}
+	}
+	pageindex[currentpage+1] = -1;  // last page
+	currentpage = 0;
+}
 
 void solid(int r, int g, int b) {
 	int i;
 	for(i=0;i<SCREEN_HEIGHT*SCREEN_WIDTH;i++) fb[i] = RGB15(r,g,b) | BIT(15);
 }
 
-void swapscreen() {
-	if(fb == screen0) fb = screen1;
-	else fb = screen0;
-}
-
 void startpage(FT_Face face) {
 	pen.y = MARGINTOP + FT_MulFix(face->size->metrics.height,face->size->metrics.x_scale)/PIXELSCALE;
 	pen.x = MARGINLEFT;
+}
+
+void clearpage() {
+	fb = screen0;
+	startpage(face);
+	solid(31,31,31);
+	fb = screen1;
+	solid(31,31,31);
+	fb = screen0;
 }
 
 void newline() {
@@ -54,7 +90,7 @@ void newline() {
 	pen.y += FT_MulFix(face->size->metrics.height,metrics.y_scale)/PIXELSCALE;
 }
 
-int draw(int code) {
+int drawchar(int code) {
 	/* x,y is in potrait page space
 	sx, sy is in screen space, rotated */
 
@@ -62,7 +98,13 @@ int draw(int code) {
 	
 	/* ignore returns */
 	if(code == '\r') return 0;
-
+	if(code == '\n') {
+		pen.x = MARGINLEFT;
+		pen.y += FT_MulFix(metrics.height, metrics.y_scale) / PIXELSCALE;
+		return 0;
+	}
+	if(code == '\t') code = ' ';
+	
 	if(FT_Load_Char(face, code, 
 		 FT_LOAD_RENDER // tell FreeType2 to render internally
 		| FT_LOAD_TARGET_NORMAL // grayscale 0-255
@@ -74,11 +116,11 @@ int draw(int code) {
 	FT_Bitmap bitmap = glyph->bitmap;
 	
 	/* space */
-	if(code == '\n' || code == ' ' || code == '\t') {
+/*	if(code == '\n' || code == ' ' || code == '\t') {
 		pen.x += FT_MulFix(glyph->advance.x, metrics.x_scale) / PIXELSCALE;
 		return 0;
 	}
-
+*/
 	/* wrap */
 	if(pen.x + bitmap.width > PAGE_WIDTH - MARGINRIGHT) {
 		pen.x = MARGINLEFT;
@@ -88,7 +130,7 @@ int draw(int code) {
 	/* off page bottom */
 	if(pen.y > PAGE_HEIGHT-MARGINBOTTOM) {
 		if(fb == screen0) {
-			swapscreen();
+			fb = screen1;
 			startpage(face);
 		}
 		else return -1;
@@ -113,22 +155,24 @@ int draw(int code) {
 	/* advance */
 	pen.x += FT_MulFix(glyph->advance.x, metrics.x_scale)/(PIXELSCALE * .75);
 
+	cursor++;
 	return 0;
 }
 
-int nextchar(FT_Face face, int code) {
-	FT_UInt index;
-	if(!code) {
-		return FT_Get_First_Char(face, &index);
+void printpage(int page) {    
+	clearpage();
+	int i;
+	for(i=pageindex[page];i<pageindex[page+1];i++) {
+//	for(i=0;i<10;i++) {
+		drawchar(buf[i]);
 	}
-	return FT_Get_Next_Char(face, code, &index);
 }
 
 void
 default_hndl(void *data, const char *s, int len) {
 	int i;
 	for(i=0;i<len;i++) {
-		draw(s[i]);
+		drawchar(s[i]);
 	}
 }  /* End default_hndl */
 
@@ -141,35 +185,24 @@ printcurrent(XML_Parser p) {
 
 void
 start_hndl(void *data, const char *el, const char **attr) {
-/*	draw('<');
-	int i;
-	for(i=0;i<strlen(el);i++) draw(el[i]);
-	draw('>'); */
 }  /* End of start_hndl */
 
 
 void
 end_hndl(void *data, const char *el) {
-/*	draw('<');
-	int i;
-	for(i=0;i<strlen(el);i++) draw(el[i]);
-	draw('/');
-	draw('>'); */
 	if(!stricmp(el,"br")
 		|| !stricmp(el,"title")
 		|| !stricmp(el,"h1")
 		|| !stricmp(el,"h2")
 		|| !stricmp(el,"h3")
 		|| !stricmp(el,"h4")
-		|| !stricmp(el,"hr")) newline();
+		|| !stricmp(el,"hr")) buf[cursor++] = '\n';
 }  /* End of end_hndl */
 
 void
 char_hndl(void *data, const char *txt, int txtlen) {
 	int i;
-	for(i=0;i<txtlen;i++) {
-		draw(txt[i]);
-	}
+	for(i=0;i<txtlen;i++) buf[cursor++] = txt[i];
 }  /* End char_hndl */
 
 void
@@ -188,7 +221,7 @@ int main(void){
 	irqEnable(IRQ_VBLANK);
 	videoSetMode(MODE_5_2D | DISPLAY_BG3_ACTIVE);
 	videoSetModeSub(MODE_5_2D | DISPLAY_BG3_ACTIVE);
-	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);    
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 	vramSetBankC(VRAM_C_SUB_BG_0x06200000);
 	
 	//initialize the background
@@ -218,41 +251,58 @@ int main(void){
         return error;
 	}
 	FT_Set_Pixel_Sizes(face, 0, PIXELSIZE);
+	// cache advances for later pagination.
+	int ascii;
+	for(ascii=0;ascii<128;ascii++) {
+		FT_Load_Char(face, ascii, 
+		 FT_LOAD_RENDER // tell FreeType2 to render internally
+		| FT_LOAD_TARGET_NORMAL); // grayscale 0-255
+		advance[ascii] = FT_MulFix(face->glyph->advance.x, face->size->metrics.x_scale) / PIXELSCALE;
+	}
 
 	XML_Parser p = XML_ParserCreate(NULL);
 	if(!p) { solid(31,0,0); return -10; }
-	XML_Char *buf = (char*)XML_GetBuffer(p, BUFSIZE);
-	
+
 	FILE *fp = fopen("/coe.htm","r");
 	if(!fp) {
 		solid(31,0,0);
-		fclose(fp);
 		return -2;
 	}
-	int bytes_read = fread(buf, 1, BUFSIZE, fp);
+	
+	struct stat s;
+//	if(fstat(fp,&s) == -1) { solid(0,31,0); fclose(fp); return -1; }
+	s.st_size = BUFSIZE;
+	
+	XML_Char *filebuf = (char*)XML_GetBuffer(p, s.st_size);
+	int bytes_read = fread(filebuf, 1, s.st_size, fp);
 	fclose(fp);
-
+	
 	XML_UseParserAsHandlerArg(p);
 	XML_SetElementHandler(p, start_hndl, end_hndl);
 	XML_SetCharacterDataHandler(p, char_hndl);
 	XML_SetProcessingInstructionHandler(p, proc_hndl);
-    
-	startpage(face);
-	solid(31,31,31);
-	swapscreen();
-	solid(31,31,31);
-	swapscreen();
 
+	cursor = 0;
 	XML_ParseBuffer(p, bytes_read, bytes_read == 0);
 	XML_ParserFree(p);
+	buf[cursor] = 0;   // terminate string
+	currentpage = 0;
+	clearpage();
+	paginate();
+	currentpage = 0;
+	printpage(currentpage);
 	
 	while(1) {
 		scanKeys();
  
-		if(keysDown() & KEY_Y) {
-			lcdSwap();
+		if(keysDown() & KEY_A) {
+			printpage(++currentpage);
 		}
-
+		
+		if(keysDown() & KEY_B) {
+			if(currentpage > 0) printpage(--currentpage);
+		}
+		
 		swiWaitForVBlank();
 	}
 	
