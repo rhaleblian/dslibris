6d08a0396de108370e5ada6583264a1408b3f90a
rhaleblian <rhaleblian>
2007-07-15 02:10:20 +0000

    converted html to xml with tiny. pagination is starting to work.

--- source/main.c
+++ source/main.c
@@ -10,132 +10,99 @@ back page
 #include <pa9.h>      /* programmer's arsenal - libnds macros */
 #include <ft2build.h> /* freetype2 - text rendering */
 #include FT_FREETYPE_H
-#include <expat.h>    /* expat - HTML parsing */
+#include <expat.h>    /* expat - XML parsing */
 #include <sys/stat.h>
 #include <unistd.h>
 	   
 #define BUFSIZE 32000
+#define PAGEBUFSIZE 1024
+#define MAXPAGES 1024
 #define MARGINLEFT 10
 #define MARGINRIGHT 10
-#define MARGINTOP 8
+#define MARGINTOP 10
 #define MARGINBOTTOM 10
-#define LINESPACING 12
-#define LETTERSPACING 6
-#define PIXELSIZE 14
-#define PIXELSCALE 32
-#define DPI 72
-#define PAGE_HEIGHT SCREEN_HEIGHT
-#define PAGE_WIDTH SCREEN_WIDTH
+#define LINESPACING 12	// cheese! use metrics
+#define LETTERSPACING 8	// cheese! use metrics
+#define PIXELSIZE 10
+#define DPI 72		    // probably not true for a DS - measure it
+#define EMTOPIXEL (float)(POINTSIZE * DPI/72.0)
+#define PAGE_HEIGHT SCREEN_WIDTH
+#define PAGE_WIDTH SCREEN_HEIGHT
 
+XML_Parser p;
 FT_Vector pen;
 u16 *screen0, *screen1, *fb;
 int Eventcnt = 0;
 FT_Library library;
 FT_Error   error;
 FT_Face    face;
-int	cursor, currentpage;
-int pageindex[2048];	// page indices into the char buffer
-int advance[128];		// glpyh advances for the current font
-char buf[BUFSIZE];
+int	curpage;
+int advance[128];		// glyph advances for the current font
+int pagedone;
+int breakingspace;     // where was the last legal char to break a line?
+int nonwhitespace;     // have we found any nonwhitespace chars on this line?
 
-int iswhitespace(char c) {
-	if(c == ' ' || c == '\t' || c == '\n') return 1;
-	return 0;
-}
+typedef struct {
+	char buf[PAGEBUFSIZE];
+	int chars;
+} page_t;
+page_t pages[MAXPAGES]; 
 
-void paginate() {
-	long i;
-	int breakable = 0;
-	int xpixels = MARGINLEFT;
-	int line = 0;
-	
-	for(i=0;i<strlen(buf);i++) {
-		if(iswhitespace(buf[i])) breakable = i;
-		xpixels += advance[(int)buf[i]];
-		if(xpixels > SCREEN_WIDTH - MARGINLEFT - MARGINRIGHT) {
-			xpixels = 0;
-			line++;
-			if(line > 12) {
-				pageindex[currentpage++] = i;
-				line = 0;
-			}
-		}
-	}
-	pageindex[currentpage+1] = -1;  // last page
-	currentpage = 0;
-}
+typedef enum {HTML,HEAD,BODY} context_t;
+context_t context;
 
 void solid(int r, int g, int b) {
 	int i;
-	for(i=0;i<SCREEN_HEIGHT*SCREEN_WIDTH;i++) fb[i] = RGB15(r,g,b) | BIT(15);
+	// TODO a memcpy() here instead
+	for(i=0;i<PAGE_HEIGHT*PAGE_HEIGHT;i++) fb[i] = RGB15(r,g,b) | BIT(15);
 }
 
-void startpage(FT_Face face) {
-	pen.y = MARGINTOP + FT_MulFix(face->size->metrics.height,face->size->metrics.x_scale)/PIXELSCALE;
-	pen.x = MARGINLEFT;
-}
-
-void clearpage() {
+void clearscreens() {
 	fb = screen0;
-	startpage(face);
 	solid(31,31,31);
 	fb = screen1;
 	solid(31,31,31);
 	fb = screen0;
 }
 
-void newline() {
-	FT_Size_Metrics metrics = face->size->metrics;
-	pen.x = MARGINLEFT;
-	pen.y += FT_MulFix(face->size->metrics.height,metrics.y_scale)/PIXELSCALE;
+void newpage(int page) {
+	strcpy(pages[page].buf,"");
+	pages[page].chars = 0;
+	fb = screen0;
 }
 
-int drawchar(int code) {
-	/* x,y is in potrait page space
-	sx, sy is in screen space, rotated */
+void startpage() {
+	fb = screen0;
+	pen.y = MARGINTOP + (face->size->metrics.ascender >> 6);
+	pen.x = MARGINLEFT;
+	pagedone = 0;
+	breakingspace = 0;
+	nonwhitespace = 0;
+}
 
-	FT_Size_Metrics metrics = face->size->metrics;
-	
-	/* ignore returns */
-	if(code == '\r') return 0;
-	if(code == '\n') {
-		pen.x = MARGINLEFT;
-		pen.y += FT_MulFix(metrics.height, metrics.y_scale) / PIXELSCALE;
-		return 0;
-	}
-	if(code == '\t') code = ' ';
-	
-	if(FT_Load_Char(face, code, 
-		 FT_LOAD_RENDER // tell FreeType2 to render internally
-		| FT_LOAD_TARGET_NORMAL // grayscale 0-255
-		)) {
-		solid(31,0,0);
-		return -2;
-	}
-	FT_GlyphSlot glyph = face->glyph;
-	FT_Bitmap bitmap = glyph->bitmap;
-	
-	/* space */
-/*	if(code == '\n' || code == ' ' || code == '\t') {
-		pen.x += FT_MulFix(glyph->advance.x, metrics.x_scale) / PIXELSCALE;
-		return 0;
-	}
-*/
-	/* wrap */
-	if(pen.x + bitmap.width > PAGE_WIDTH - MARGINRIGHT) {
-		pen.x = MARGINLEFT;
-		pen.y += FT_MulFix(metrics.height, metrics.y_scale) / PIXELSCALE;
-	}
-	
-	/* off page bottom */
-	if(pen.y > PAGE_HEIGHT-MARGINBOTTOM) {
+void newline() {
+	int linespacing = face->size->metrics.height >> 6;
+	if(pen.y + linespacing > PAGE_HEIGHT - MARGINBOTTOM) {
+		// next screen or end of page
 		if(fb == screen0) {
 			fb = screen1;
-			startpage(face);
+			pen.y = MARGINTOP + (face->size->metrics.ascender >> 6);
+		} else {
+			pagedone = 1;
 		}
-		else return -1;
-	}
+	} else pen.y += linespacing;
+	pen.x = MARGINLEFT;
+	nonwhitespace = 0;
+	breakingspace = 0;
+}
 
+void drawchar(int code) {
+	// TODO slow! cache this data instead.
+	FT_Load_Char(face, code, FT_LOAD_RENDER | FT_LOAD_TARGET_NORMAL);
+	FT_GlyphSlot glyph = face->glyph;
+	FT_Bitmap bitmap = glyph->bitmap;
+	int bx = glyph->bitmap_left;
+	int by = glyph->bitmap_top;
 	int gx, gy;
 	for(gy=0; gy<bitmap.rows; gy++) {
 		for(gx=0; gx<bitmap.width; gx++) {
@@ -143,66 +110,98 @@ int drawchar(int code) {
 			int a = bitmap.buffer[gy*bitmap.width+gx];
 			int l = (0*a + (255-a));
 
-			u8 sx = (pen.x+gx+glyph->bitmap_left);
-			u8 sy = (pen.y+gy-glyph->bitmap_top);			
+			u8 sx = (pen.x+gx+bx);
+			u8 sy = (pen.y+gy-by);			
 			fb[sy*SCREEN_WIDTH+sx] = RGB15(l>>3,l>>3,l>>3) | BIT(15);
 		}
 	}
-	
-	/* mark the origin */
-//	fb[pen.y*PAGE_WIDTH+pen.x] = RGB15(31,0,0) | BIT(15);
-	
-	/* advance */
-	pen.x += FT_MulFix(glyph->advance.x, metrics.x_scale)/(PIXELSCALE * .75);
-
-	cursor++;
-	return 0;
 }
 
-void printpage(int page) {    
-	clearpage();
+void writepage(int p) {
+	startpage();
+	clearscreens();
 	int i;
-	for(i=pageindex[page];i<pageindex[page+1];i++) {
-//	for(i=0;i<10;i++) {
-		drawchar(buf[i]);
+	for(i=0;i<pages[p].chars;i++) {
+		int c = (int)pages[p].buf[i];
+		if(c == '\n') newline();
+		else {
+			drawchar((int)(pages[p].buf[i]));
+			pen.x += advance[(int)(pages[curpage].buf[i])];
+		}
+		if(pagedone) return;
 	}
 }
 
 void
 default_hndl(void *data, const char *s, int len) {
-	int i;
-	for(i=0;i<len;i++) {
-		drawchar(s[i]);
-	}
 }  /* End default_hndl */
 
 void
-printcurrent(XML_Parser p) {
-  XML_SetDefaultHandler(p, default_hndl);
-  XML_DefaultCurrent(p);
-  XML_SetDefaultHandler(p, (XML_DefaultHandler) 0);
-}  /* End printcurrent */
-
-void
 start_hndl(void *data, const char *el, const char **attr) {
+	if(!stricmp(el,"html")) context = HTML;
+	if(!stricmp(el,"body")) context = BODY;
+	if(!stricmp(el,"head")) context = HEAD;
 }  /* End of start_hndl */
-
 
 void
 end_hndl(void *data, const char *el) {
-	if(!stricmp(el,"br")
-		|| !stricmp(el,"title")
+	page_t *page = &pages[curpage];
+	if(
+		!stricmp(el,"title")
 		|| !stricmp(el,"h1")
 		|| !stricmp(el,"h2")
 		|| !stricmp(el,"h3")
 		|| !stricmp(el,"h4")
-		|| !stricmp(el,"hr")) buf[cursor++] = '\n';
+		|| !stricmp(el,"hr")
+		|| !stricmp(el,"p")
+		|| !stricmp(el,"br")
+	) {
+		page->buf[page->chars] = '\n';
+		page->chars++;
+		newline();
+		if(pagedone) {
+			curpage++;
+			newpage(curpage);
+			startpage();
+		}
+	}
 }  /* End of end_hndl */
 
 void
 char_hndl(void *data, const char *txt, int txtlen) {
-	int i;
-	for(i=0;i<txtlen;i++) buf[cursor++] = txt[i];
+	// TODO txt is UTF-8, but for now we assume it's always in the ASCII range.
+	
+	if(context != BODY) return;
+	
+	page_t *page = &pages[curpage];
+	int i=0;
+	while(i<txtlen) {
+		int c = (int)txt[i];
+		if(c > 127) continue;  // UTF-8...
+		if(c == '\r') continue;
+		if(c == '\t') c = ' ';
+		if(c == '\n') c = ' ';
+
+		if(c == ' ') {
+			breakingspace = page->chars;
+			if(!nonwhitespace) { i++; continue; } // don't add leading space
+		} else nonwhitespace = 1;
+		
+		page->buf[page->chars] = c;
+		page->chars++;
+		if((pen.x + advance[c]) > (132)) {
+			page->buf[breakingspace] = '\n';
+			newline();
+			if(pagedone) {
+				curpage++;
+				newpage(curpage);
+				startpage();
+				page++;
+			}
+		} else 	pen.x += advance[c];
+
+		i++;
+	}
 }  /* End char_hndl */
 
 void
@@ -210,9 +209,7 @@ proc_hndl(void *data, const char *target, const char *pidata) {
 }  /* End proc_hndl */
 
 int main(void){		
-	screen0 = (u16*)BG_BMP_RAM(0);
-	screen1 = (u16*)BG_BMP_RAM_SUB(0);
-	fb = screen0;
+	int pagecount = 0;
 	
 	fatInitDefault();
 	powerON(POWER_ALL);
@@ -223,84 +220,122 @@ int main(void){
 	videoSetModeSub(MODE_5_2D | DISPLAY_BG3_ACTIVE);
 	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 	vramSetBankC(VRAM_C_SUB_BG_0x06200000);
-	
-	//initialize the background
-	BACKGROUND.control[3] = BG_BMP16_256x256 | BG_BMP_BASE(0);
-	
-	BACKGROUND.bg3_rotation.xdy = 0;
-	BACKGROUND.bg3_rotation.xdx = 1 << 8;
-	BACKGROUND.bg3_rotation.ydx = 0;
-	BACKGROUND.bg3_rotation.ydy = 1 << 8;
- 
-	//initialize the sub background
-	BACKGROUND_SUB.control[3] = BG_BMP16_256x256 | BG_BMP_BASE(0);
-	
-	BACKGROUND_SUB.bg3_rotation.xdy = 0;
-	BACKGROUND_SUB.bg3_rotation.xdx = 1 << 8;
-	BACKGROUND_SUB.bg3_rotation.ydx = 0;
-	BACKGROUND_SUB.bg3_rotation.ydy = 1 << 8;
 
+	screen0 = (u16*)BG_BMP_RAM(0);
+	screen1 = (u16*)BG_BMP_RAM_SUB(0);
+	clearscreens();
+	
+	//initialize the backgrounds
+    s16 s = SIN[-128 & 0x1FF] >> 4;
+	s16 c = COS[-128 & 0x1FF] >> 4;
+	BACKGROUND.control[3] = BG_BMP16_256x256 | BG_BMP_BASE(0);
+	BG3_XDX = c;
+	BG3_XDY = -s;
+	BG3_YDX = s;
+	BG3_YDY = c;
+	BG3_CX = 0 << 8;
+	BG3_CY = 256 << 8;
+	BACKGROUND_SUB.control[3] = BG_BMP16_256x256 | BG_BMP_BASE(0);
+	SUB_BG3_XDX = c;
+	SUB_BG3_XDY = -s;
+	SUB_BG3_YDX = s;
+	SUB_BG3_YDY = c;
+	SUB_BG3_CX = 0 << 8;
+	SUB_BG3_CY = 256 << 8;
+
+	// font.
+	
     error = FT_Init_FreeType(&library);
     if(error) {
-		solid(31,31,0);
+		solid(31,0,0);
         return error;
 	}
 	error = FT_New_Face(library, "/frutiger.ttf", 0, &face);
 	if(error) {
-		solid(0,0,31);
+		solid(31,0,0);
         return error;
 	}
 	FT_Set_Pixel_Sizes(face, 0, PIXELSIZE);
-	// cache advances for later pagination.
-	int ascii;
-	for(ascii=0;ascii<128;ascii++) {
-		FT_Load_Char(face, ascii, 
-		 FT_LOAD_RENDER // tell FreeType2 to render internally
+	
+	// cache advances, for pagination pass.
+	// using FirstChar() and NextChar() would be more robust.
+	// TODO also cache bitmaps, kerning and transformations.
+	int i;
+	for(i=0;i<128;i++) {
+		FT_Load_Char(face, i, 
+		FT_LOAD_RENDER // tell FreeType2 to render internally
 		| FT_LOAD_TARGET_NORMAL); // grayscale 0-255
-		advance[ascii] = FT_MulFix(face->glyph->advance.x, face->size->metrics.x_scale) / PIXELSCALE;
+		advance[i] = face->glyph->advance.x >> 6;
 	}
 
-	XML_Parser p = XML_ParserCreate(NULL);
+	// parser.
+
+	p = XML_ParserCreate(NULL);
 	if(!p) { solid(31,0,0); return -10; }
-
-	FILE *fp = fopen("/coe.htm","r");
-	if(!fp) {
-		solid(31,0,0);
-		return -2;
-	}
-	
-	struct stat s;
-//	if(fstat(fp,&s) == -1) { solid(0,31,0); fclose(fp); return -1; }
-	s.st_size = BUFSIZE;
-	
-	XML_Char *filebuf = (char*)XML_GetBuffer(p, s.st_size);
-	int bytes_read = fread(filebuf, 1, s.st_size, fp);
-	fclose(fp);
-	
 	XML_UseParserAsHandlerArg(p);
 	XML_SetElementHandler(p, start_hndl, end_hndl);
 	XML_SetCharacterDataHandler(p, char_hndl);
 	XML_SetProcessingInstructionHandler(p, proc_hndl);
 
-	cursor = 0;
-	XML_ParseBuffer(p, bytes_read, bytes_read == 0);
+#ifdef DEBUG
+	swiWaitForVBlank();
+	solid(0,0,31);		
+#endif
+
+	// file. must be well-formed XML or XHTML.
+	// UVA HTML texts, for instance, need to go through HTML tidy.
+	
+	FILE *fp = fopen("/rhetorica.xml","r");
+	if(!fp) {
+		solid(31,0,0);
+		return -2;
+	}
+	struct stat st;
+	if(fstat((int)fp,&st) == -1) st.st_size = BUFSIZE;
+	XML_Char *filebuf = (char*)XML_GetBuffer(p, st.st_size);
+	int bytes_read = fread(filebuf, 1, st.st_size, fp);
+	fclose(fp);
+
+#ifdef DEBUG
+	fb = screen1;
+	swiWaitForVBlank();
+	solid(0,31,0);
+	fb = screen0;
+#endif
+
+	// parse and paginate.
+	
+	curpage = 0;
+	newpage(curpage);
+	nonwhitespace = 0;
+	error = XML_ParseBuffer(p, bytes_read, bytes_read == 0);
 	XML_ParserFree(p);
-	buf[cursor] = 0;   // terminate string
-	currentpage = 0;
-	clearpage();
-	paginate();
-	currentpage = 0;
-	printpage(currentpage);
+	pagecount = curpage-1;
+	
+#ifdef DEBUG
+	swiWaitForVBlank();
+	solid(0,31,0);
+#endif
+	
+	//draw page 0
+	
+	curpage = 0;
+	writepage(curpage);
+	swiWaitForVBlank();
 	
 	while(1) {
 		scanKeys();
  
-		if(keysDown() & KEY_A) {
-			printpage(++currentpage);
+		if((keysDown() & KEY_A) || (keysDown() & KEY_R)) {
+			curpage++;
+			writepage(curpage);
 		}
 		
-		if(keysDown() & KEY_B) {
-			if(currentpage > 0) printpage(--currentpage);
+		if((keysDown() & KEY_B) || (keysDown() & KEY_L)) {
+			if(curpage > 0) {
+				curpage--;
+				writepage(curpage);
+			}
 		}
 		
 		swiWaitForVBlank();
