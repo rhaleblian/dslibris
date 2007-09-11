5d12ec19111bfaab2d03f91d325044d001448dac
rhaleblian <rhaleblian>
2007-09-11 04:00:01 +0000

    commented out PNG and JPG code.

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -1,5 +1,5 @@
 /**----------------------------------------------------------------------------
-   $Id: main.c,v 1.14 2007/09/04 06:34:17 rhaleblian Exp $
+   $Id: main.c,v 1.15 2007/09/10 14:08:22 rhaleblian Exp $
    dslibris - an ebook reader for Nintendo DS
    -------------------------------------------------------------------------**/
 
@@ -17,8 +17,8 @@
 #include <errno.h>
 
 #include <expat.h>
-#include <jpeglib.h>
-#include <png.h>
+//#include <jpeglib.h>
+//#include <png.h>
 
 #include "main.h"
 #include "parse.h"
@@ -94,35 +94,44 @@ void prefs_write(void);
 
 void spin(void) { while(true) swiWaitForVBlank(); }
 
+/*
+void user_error_ptr() {}
+void user_error_fn() {}
+void user_warning_fn() {}
+
 #define ERROR 1
 u8 splash(void)
 {
   char file_name[32] = "dslibris.png";
   FILE *fp = fopen(file_name, "rb");
 
-   png_structp png_ptr = png_create_read_struct
-       (PNG_LIBPNG_VER_STRING, (png_voidp)user_error_ptr,
-        user_error_fn, user_warning_fn);
-    if (!png_ptr)
-        return (ERROR);
+  png_structp png_ptr = png_create_read_struct
+    (PNG_LIBPNG_VER_STRING, (png_voidp)user_error_ptr,
+     user_error_fn, user_warning_fn);
+  if (!png_ptr)
+    return (1);
 
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr)
+  png_infop info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr)
     {
-        png_destroy_read_struct(&png_ptr,
-           (png_infopp)NULL, (png_infopp)NULL);
-        return (ERROR);
+      png_destroy_read_struct(&png_ptr,
+			      (png_infopp)NULL, (png_infopp)NULL);
+      return (2);
     }
-
-    png_infop end_info = png_create_info_struct(png_ptr);
-    if (!end_info)
+  
+  png_infop end_info = png_create_info_struct(png_ptr);
+  if (!end_info)
     {
-        png_destroy_read_struct(&png_ptr, &info_ptr,
-          (png_infopp)NULL);
-        return (ERROR);
+      png_destroy_read_struct(&png_ptr, &info_ptr,
+			      (png_infopp)NULL);
+      return (3);
     }
+  
   fclose(fp);
+  return(0);
 }
+*/
+
 
 int main(void) {
   bool browseractive = false;
