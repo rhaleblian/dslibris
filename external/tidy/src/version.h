ec94863a5df98f9f198da561853812ea5c3429db
rayh23 <rayh23>
2007-11-10 02:43:46 +0000

    added tidylib library.

---
+++ external/tidy/src/version.h
@@ -0,0 +1,14 @@
+/* version information
+
+  (c) 2007 (W3C) MIT, ERCIM, Keio University
+  See tidy.h for the copyright notice.
+
+  CVS Info :
+
+    $Author: arnaud02 $ 
+    $Date: 2007/09/19 12:51:24 $ 
+    $Revision: 1.37 $ 
+
+*/
+
+static const char TY_(release_date)[] = "19 September 2007";
