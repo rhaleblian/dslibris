6ce04dd7408348c3625029a407dd914b1eea1853
rayh23 <rayh23>
2009-09-12 11:14:18 +0000

    adds old scripts for cleaning HTML from various sources.

---
+++ data/tool/acrocruft.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# remove cruft from HTML3 saved by Acrobat 7.
+import re
+import sys
+
+infile = sys.argv[1]
+fp = open(infile,'r')
+buffer = fp.read()
+p = re.compile('<BR>')
+buffer = p.sub(' ',buffer)
+p = re.compile('</?SPAN[^>]*>')
+buffer = p.sub(' ',buffer)
+print buffer
+fp.close()
+
---
+++ data/tool/gutenluck.app/Contents/Resources/gutenluck.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+##! Convert a file or URL of a PG ASCII file to XHTML.
+##  Requires GutenMark and HTML Tidy.
+##  usage: gutenluck.py <url> [outfile]
+##
+## <site-configuration>
+path_gut_program_root = "/Applications/GUItenMark.app/Contents/MacOS/"
+path_gut_config_root = "/Applications/GUItenMark.app/Contents/Resources/GutenMark.cfg"
+path_tidy_program_root = ""
+## </site-configuration>
+
+import os, sys, urllib
+from subprocess import *
+
+infile = sys.argv[1]
+outfile = '-'
+if len(sys.argv) > 2:
+    outfile = sys.argv[2]
+
+# For local files, infile remains unchanged. Nice.
+(infile,headers) = urllib.urlretrieve(infile)
+print 'infile: ' + infile
+
+p1 = Popen([path_gut_program_root + "GutenMark",
+            "--config=" + path_gut_config_root,
+            infile],
+           stdout=PIPE)
+
+tidy_cmd = [path_tidy_program_root + 'tidy',            
+            '-asxhtml',
+            '-numeric']
+if outfile != '-':
+    tidy_cmd.append("-o")
+    tidy_cmd.append(outfile)
+
+p2 = Popen(tidy_cmd,stdin=p1.stdout)
+p2.communicate()[0]
