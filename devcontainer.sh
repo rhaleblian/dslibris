46cc3e334e66c59c00003f8498e5279cc66e0e30
Ray Haleblian <ray@haleblian.com>
2025-03-11 22:18:52 +0000

    Add setup for a Codespace

---
+++ devcontainer.sh
@@ -0,0 +1,6 @@
+wget https://apt.devkitpro.org/install-devkitpro-pacman
+chmod +x install-devkitpro-pacman 
+sudo ./install-devkitpro-pacman 
+rm ./install-devkitpro-pacman
+sudo dkp-pacman -S $(cat requirements.txt)
+
